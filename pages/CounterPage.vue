1cc0fa7f7cbe7d96fae8cbda4f3e6fc60762639c
acaibawl <hagiya0719@gmail.com>
2022-06-10 18:40:39 +0900

    counter moduleを使った簡単なstore操作の学習

---
+++ pages/CounterPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    Clicked: {{ $store.state.counter.counterModule.count }} times, count is {{ evenOrOdd }}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions} from "vuex";
+
+export default {
+  name: "CounterPage",
+  computed: mapGetters('counter/counterModule', [
+    'evenOrOdd'
+  ]),
+  methods: mapActions('counter/counterModule', [
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+  ])
+}
+</script>
+
+<style scoped>
+button {
+  display: block;
+  border: orange solid 3px;
+  padding: 3px;
+}
+</style>
